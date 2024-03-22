c72194edf2e443419ef12d6555ec848e2d3f0096
SnehalSalve <mauu7878@email.com>
2024-03-22 23:08:32 +0530

    update a function name

--- 3My-work.c
+++ 3My-work.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 //missing a number an array
-void OKFind(int a[],int n){
+void missingNumber(int a[],int n){
 
     int i,search;
     search = (n+1)*(n+2)/2;
@@ -20,6 +20,6 @@ int main(){
     //sizeof(Array_name): It is an operator that returns the size of the entire array in bytes.
     //sizeof(Array_name[index]): It returns the size of a single element in the array in bytes.
     int size = sizeof(arr)/sizeof(arr[0]);
-    OKFind(arr,size);
+    missingNumber(arr,size);
     return 0;
 }
